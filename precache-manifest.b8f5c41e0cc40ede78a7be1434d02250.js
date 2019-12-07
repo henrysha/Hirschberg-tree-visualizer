519d3b2ddca3ca578948bf037b0b7adc91c9e5a6
Henry Ha <sha16@illinois.edu>
2019-12-06 21:40:56 -0600

    Updates

---
+++ precache-manifest.b8f5c41e0cc40ede78a7be1434d02250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e196f203d64e3576107735a508af0e0",
+    "url": "/Hirschberg-tree-visualizer/index.html"
+  },
+  {
+    "revision": "a9825f2d11659e64e0f6",
+    "url": "/Hirschberg-tree-visualizer/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "4dc8e577d20b0714c296",
+    "url": "/Hirschberg-tree-visualizer/static/css/main.f623a457.chunk.css"
+  },
+  {
+    "revision": "a9825f2d11659e64e0f6",
+    "url": "/Hirschberg-tree-visualizer/static/js/2.45acbb2d.chunk.js"
+  },
+  {
+    "revision": "4dc8e577d20b0714c296",
+    "url": "/Hirschberg-tree-visualizer/static/js/main.d63738b9.chunk.js"
+  },
+  {
+    "revision": "119451fc4e6c937ce181",
+    "url": "/Hirschberg-tree-visualizer/static/js/runtime-main.5adaa362.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Hirschberg-tree-visualizer/precache-manifest.e47666ee2260be6899c938b71dedfc48.js"
+  "/Hirschberg-tree-visualizer/precache-manifest.b8f5c41e0cc40ede78a7be1434d02250.js"
 );
 
 self.addEventListener('message', (event) => {
