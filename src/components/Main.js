e3ccb3773b3efa8066923d5577fbd40e6aafc4f2
Henry Ha <sha16@illinois.edu>
2019-12-06 21:40:37 -0600

    added alignment output

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import InputForm from "./InputForm";
 import RecursionTree from "./Tree";
 import { h } from "../Hirschberg";
+import "./Main.css";
 
 class Main extends Component {
   constructor(props) {
@@ -13,7 +14,8 @@ class Main extends Component {
       mismatchScore: 0,
       gapScore: 0,
       displayResult: false,
-      treeData: {}
+      treeData: {},
+      alignment: ""
     };
     this.submitInput = this.submitInput.bind(this);
   }
@@ -25,22 +27,41 @@ class Main extends Component {
     var hResult = h(inputObj.sequence1, inputObj.sequence2);
     console.log("hResult: ", hResult);
     this.setState(inputObj);
-    this.setState({ treeData: hResult[1] });
+    this.setState({
+      treeData: hResult[1],
+      alignment: hResult[0]
+    });
   }
 
   render() {
     console.log("state in main: ", this.state);
     console.log("displayResult: ", this.state.displayResult);
-    // const text = this.state.displayResult ? 'output here' : null
+    const text = this.state.alignment;
     let tree = null;
+    let alignment_header = "";
+    let alignment = null;
     if (this.state.displayResult) {
       console.log("curr state: ", this.state);
       tree = <RecursionTree data={this.state.treeData} />;
+      alignment_header = "alignment";
+      alignment = (
+        <div className="alignment">
+          <span>{alignment_header}</span>
+          {text.split("\n").map((i, key) => {
+            return (
+              <div>
+                <code key={key}>{i}</code>
+              </div>
+            );
+          })}
+        </div>
+      );
     }
 
     return (
       <div>
         <InputForm submitInput={this.submitInput} />
+        {alignment}
         {tree}
       </div>
     );
