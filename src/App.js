79ec33ab92aa98ad6d66d52d1d41ef13fbe8500a
Isabell L <il3@illinois.edu>
2019-12-17 13:10:15 -0600

    added description

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Main from './components/Main';
 
 function App() {
   const title = 'Hirschberg\'s Algorithm Visualization';
+  const description = 'Hirschberg\'s algorithm is a dynamic programming algorithm that finds the optimal sequence alignment between two strings. Hirschberg modifies global alignment (Needleman–Wunsch algorithm) to using only linear space. Hirschberg uses a divide and conquer approach to partition each problem into two smaller subproblems. Hirschberg is based on the observation that to compute the optimal alignment score, we only need to store the current and previous row of the dynamic programming score matrix of Needleman-Wunsch. ';
   return (
     <div className="App">
       <h2 className='title'>
         {title}
       </h2>
+      <h1 className='description'>
+        {description}
+      </h1>
       <div className='title'>
         <Main/>
       </div>
